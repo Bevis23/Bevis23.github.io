3d9ae39f7d54fed8c797debd376b42c0d6367342
Bevis23 <miyi23seek@gmail.com>
2023-05-28 21:09:46 +0800

    Site updated: 2023-05-28 21:09:42

--- js/matery.js
+++ js/matery.js
@@ -139,3 +139,12 @@ $(function () {
         }
     });
 });
+
+//黑夜模式提醒开启功能
+setTimeout(function () {
+    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用黑夜模式阅读能够减轻视觉疲劳。</span>'
+        M.toast({ html: toastHTML })
+    }
+}, 2000)
+    
