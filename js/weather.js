44ee6259a3af3ee3c9643f13943defd658a0a9cf
Bevis23 <miyi23seek@gmail.com>
2023-05-28 15:08:44 +0800

    Site updated: 2023-05-28 15:08:43

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+<div id="he-plugin-simple"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "000000",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "10",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "0c23555efe48479490a674b8fd98f4c4"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
